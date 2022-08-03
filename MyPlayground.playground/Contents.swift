f39378baa3f17a01a20efb31ae144e5117fe3145
basmah saad <basmahsaad@basmahs-MacBook-Air.local>
2022-08-03 19:44:53 +0300

    KK

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+
+var name = "basmah"
+var favouritenumber = 23
+var hobby = "drawing"
+
+let BirthOfDate = 2007
+
+print("my name is \(name) my favourite number is \(favouritenumber), i love \(hobby) \n and i was born in \(BirthOfDate)")
+
