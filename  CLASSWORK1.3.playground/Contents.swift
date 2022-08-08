a8c10bc73d19f13ead63ba4e7ea5a07b28e9d050
basmah saad <basmahkw7@icloud.com>
2022-08-08 16:14:26 +0300

    b

---
+++  CLASSWORK1.3.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var Name = "basmah"
+var hobby = "drawing"
+var favouriteNumber = 23
+
+let BirthOfDate = 2007
+
+print("hello my name is \(Name), My favourite Number is \(favouriteNumber) and i love \(hobby) \n i was born in \(BirthOfDate)")
